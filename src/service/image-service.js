86b1719845d39066cbe3a4275feaf52eb784abdb
NikitaSalnykov <salnikov.nkt@gmail.com>
2023-08-12 12:49:38 +0300

    complete #1 task

--- src/service/image-service.js
+++ src/service/image-service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = '';
+const API_KEY = 'oOo6Lam1Q3bwjUE4NKHcYE9Za8PayqnJJ1Yp0aR3Fex19EO7kjXQmTyU';
 axios.defaults.baseURL = 'https://api.pexels.com/v1/';
 axios.defaults.headers.common['Authorization'] = API_KEY;
 axios.defaults.params = {
@@ -8,4 +8,7 @@ axios.defaults.params = {
   per_page: 15,
 };
 
-export const getImages = (query, page) => {};
+export const getImages = async (query, page) => {
+  const { data } = await axios.get(`search?query=${query}&page=${page}`);
+  return data;
+};
--- src/tabs/Gallery.jsx
+++ src/tabs/Gallery.jsx
@@ -4,10 +4,60 @@ import * as ImageService from 'service/image-service';
 import { Button, SearchForm, Grid, GridItem, Text, CardItem } from 'components';
 
 export class Gallery extends Component {
+  state = {
+    photos: [],
+    value: '',
+    page: 1,
+    loadMore: false,
+  };
+
+  componentDidUpdate(_, prevState) {
+    const { value, page } = this.state;
+    const fetchData = async () => {
+      const {
+        photos,
+        total_results,
+        per_page,
+        page: currentPage,
+      } = await ImageService.getImages(value, page);
+      this.setState(prevState => ({
+        photos: [...prevState.photos, ...photos],
+        loadMore: currentPage < Math.ceil(total_results / per_page),
+      }));
+    };
+
+    if (prevState.value !== value || prevState.page !== page) {
+      fetchData();
+    }
+  }
+
+  handleSubmit = value => {
+    this.setState({ value, page: 1, photos: [] });
+  };
+
+  loadMorePhotos = () => {
+    this.setState(prevState => ({ page: prevState.page + 1 }));
+  };
+
   render() {
+    console.log(this.state.photos);
+    const { photos, loadMore } = this.state;
     return (
       <>
-        <Text textAlign="center">Sorry. There are no images ... 😭</Text>
+        <SearchForm handleSubmit={this.handleSubmit} />
+        <Grid>
+          {photos.map(({ id, avg_color, alt, src }) => (
+            <GridItem key={id}>
+              <CardItem color={avg_color}>
+                <img src={src.large} alt={alt} />
+              </CardItem>
+            </GridItem>
+          ))}
+        </Grid>
+        {loadMore && <Button onClick={this.loadMorePhotos}>Load More</Button>}
+        {photos.length === 0 && (
+          <Text textAlign="center">Sorry. There are no images ... 😭</Text>
+        )}
       </>
     );
   }
